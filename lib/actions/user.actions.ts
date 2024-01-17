e76ff080986f23cc2126b9b92bb51c5b1b27e775
smudgy-g <adamgriff86@gmail.com>
2024-01-17 15:17:40 +0100

    db: define models

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,82 @@
+'user server'
+
+import { CreateUserParams, UpdateUserParams } from '@/types'
+import { handleError } from '../utils'
+import { connectToDB } from '../database'
+import User from '../database/models/user.model'
+import Event from '../database/models/event.model'
+import Order from '../database/models/order.model'
+import { revalidatePath } from 'next/cache'
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    await connectToDB()
+
+    const newUser = await User.create(user)
+    return JSON.parse(JSON.stringify(newUser))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+export const getUserById = async (userId: string) => {
+  try {
+    await connectToDB()
+
+    const user = await User.findById(userId)
+
+    if (!user) throw new Error('User not found.')
+
+    return JSON.parse(JSON.stringify(user))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+export const updateUser = async (clerkId: string, user: UpdateUserParams) => {
+  try {
+    await connectToDB()
+
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+      new: true,
+    })
+
+    if (!updatedUser) throw new Error('User update failed.')
+
+    return JSON.parse(JSON.stringify(updatedUser))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+export const deleteUser = async (clerkId: string) => {
+  try {
+    await connectToDB()
+
+    const userToDelete = await User.findOne({ clerkId })
+
+    if (!userToDelete) throw new Error('User not found.')
+
+    // Unlink the relationships
+    await Promise.all([
+      // remove references to the user in 'events' collection
+      Event.updateMany(
+        { _id: { $in: userToDelete.events } },
+        { $pull: { organiser: userToDelete._id } }
+      ),
+
+      // update the orders collection to remove references to the user
+      Order.updateMany([
+        { _id: { $in: userToDelete.orders } },
+        { $unset: { buyer: 1 } },
+      ]),
+    ])
+
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id)
+    revalidatePath('/')
+
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
+  } catch (error) {
+    handleError(error)
+  }
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,105 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+
+import { twMerge } from 'tailwind-merge'
+import qs from 'query-string'
+
+import { UrlQueryParams, RemoveUrlQueryParams } from '@/types'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const formatDateTime = (dateString: Date) => {
+  const dateTimeOptions: Intl.DateTimeFormatOptions = {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  }
+
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: 'short',
+    month: 'short',
+    year: 'numeric',
+    day: 'numeric',
+  }
+
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: 'numeric', 
+    minute: 'numeric', 
+    hour12: true, 
+  }
+
+  const formattedDateTime: string = new Date(dateString).toLocaleString(
+    'en-US',
+    dateTimeOptions
+  )
+
+  const formattedDate: string = new Date(dateString).toLocaleString(
+    'en-US',
+    dateOptions
+  )
+
+  const formattedTime: string = new Date(dateString).toLocaleString(
+    'en-US',
+    timeOptions
+  )
+
+  return {
+    dateTime: formattedDateTime,
+    dateOnly: formattedDate,
+    timeOnly: formattedTime,
+  }
+}
+
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file)
+
+export const formatPrice = (price: string) => {
+  const amount = parseFloat(price)
+  const formattedPrice = new Intl.NumberFormat('de-DE', {
+    style: 'currency',
+    currency: 'EUR',
+  }).format(amount)
+
+  return formattedPrice
+}
+
+export function formUrlQuery({ params, key, value }: UrlQueryParams) {
+  const currentUrl = qs.parse(params)
+
+  currentUrl[key] = value
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  )
+}
+
+export function removeKeysFromQuery({
+  params,
+  keysToRemove,
+}: RemoveUrlQueryParams) {
+  const currentUrl = qs.parse(params)
+
+  keysToRemove.forEach((key) => {
+    delete currentUrl[key]
+  })
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  )
+}
+
+export const handleError = (error: unknown) => {
+  console.error(error)
+  throw new Error(typeof error === 'string' ? error : JSON.stringify(error))
 }
