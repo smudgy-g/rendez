f975baca90784273a7aabab7d67fbf93bedba2c5
smudgy-g <adamgriff86@gmail.com>
2024-01-22 19:32:04 +0100

    feat: checkout with stripe

---
+++ components/shared/CheckoutButton.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { IEvent } from '@/lib/database/models/event.model'
+import { SignedIn, SignedOut, useUser } from '@clerk/nextjs'
+import React from 'react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import Checkout from './Checkout'
+type CheckoutButtonProps = {
+  event: IEvent
+}
+
+const CheckoutButton = ({ event }: CheckoutButtonProps) => {
+  const isEventFinished = new Date(event.endDateTime) < new Date()
+  const {user} = useUser()
+  const userId = user?.publicMetadata.userId as string
+
+  return (
+    <div className="flex items-center gap-3">
+    {isEventFinished ? (
+      <p className="p-2 text-red-400">Sorry, tickets are no longer available.</p>
+    ): (
+      <>
+        <SignedOut>
+          <Button asChild className="button rounded-lg" size="lg">
+            <Link href="/sign-in">
+              Get Tickets
+            </Link>
+          </Button>
+        </SignedOut>
+
+        <SignedIn>
+          <Checkout event={event} userId={userId} />
+        </SignedIn>
+      </>
+    )}
+  </div>
+  )
+}
+
+export default CheckoutButton
--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,16 +1,72 @@
-import { GetOrdersByUserParams } from '@/types'
+'use server'
+
+import {
+  CheckoutOrderParams,
+  CreateOrderParams,
+  GetOrdersByUserParams,
+} from '@/types'
 import { handleError } from '../utils'
 import { connectToDB } from '../database'
 import Order from '../database/models/order.model'
 import Event from '../database/models/event.model'
-import { ObjectId } from 'mongodb'
 import User from '../database/models/user.model'
+import Stripe from 'stripe'
+import { redirect } from 'next/navigation'
 
-export async function getOrdersByUser({
+export const checkoutOrder = async (order: CheckoutOrderParams) => {
+  const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
+
+  const price = order.isFree ? 0 : Number(order.price) * 100
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: 'eur',
+            unit_amount: price,
+            product_data: {
+              name: order.eventTitle,
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        eventId: order.eventId,
+        buyerId: order.buyerId,
+      },
+      mode: 'payment',
+      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+      cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+    })
+    redirect(session.url!)
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+export const createOrder = async (order: CreateOrderParams) => {
+  try {
+    await connectToDB()
+
+    const newOrder = await Order.create({
+      ...order,
+      event: order.eventId,
+      buyer: order.buyerId,
+    })
+
+    return JSON.parse(JSON.stringify(newOrder))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+export const getOrdersByUser = async ({
   userId,
   limit = 3,
   page,
-}: GetOrdersByUserParams) {
+}: GetOrdersByUserParams) => {
   try {
     await connectToDB()
 
